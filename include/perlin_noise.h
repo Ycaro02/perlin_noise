a8274fedd4fd22d64cda132c3a97f8aa3e3e0fc8
Ycaro02 <Ycaro02@hotmail.fr>
2024-06-24 21:19:35 +0200

    Update interpolateNoiseGet, remove PerlinData from args and add width height for portabily

--- include/perlin_noise.h
+++ include/perlin_noise.h
@@ -167,8 +167,8 @@ u8 **perlinToSnakeData(f32 **perlinData, int w, int h);
 f32 normalisef32Tof32(f32 value, f32 start1, f32 stop1, f32 start2, f32 stop2);
 f32 perlinInterpolate(f32 a, f32 b, f32 t);
 f32 bilinearInterpolation(f32 q11, f32 q12, f32 q21, f32 q22, f32 x, f32 z);
-f32 getInterpolatedPerlinNoise(f32 **perlinNoise, f32 x, f32 z, f32 scale, s32 width, s32 height, PerlinData *perlinVal);
-f32 interpolateNoiseGet(f32 **perlinNoise, s32 x, s32 z, PerlinData *perlinVal, f32 scaleFactor);
+f32 getInterpolatedPerlinNoise(f32 **perlinNoise, f32 x, f32 z, f32 scale, s32 width, s32 height);
+f32 interpolateNoiseGet(f32 **perlinNoise, s32 x, s32 z, f32 scaleFactor, s32 width, s32 height);
 
 f32 **array1DTo2D(u8 *array, u32 height, u32 width);
 f32 normalizeU8Tof32(u8 value, u8 start1, u8 stop1, f32 start2, f32 stop2);
